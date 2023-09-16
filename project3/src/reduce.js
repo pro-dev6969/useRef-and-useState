92a027baa49f4149a574281c5f16b4b475f9fce3
Vedant Bhardwaj <93377555+pro-dev6969@users.noreply.github.com>
2023-09-16 15:12:56 +0530

    Update reduce.js

--- project3/src/reduce.js
+++ project3/src/reduce.js
@@ -2,10 +2,10 @@ import cartItems from "./cart";
 
 const reduce =(state,action) =>{
     if(action.type==='remove'){
-        return(state.filter((item)=>item.id !== action.id))
+        return{...state , data : state.data.filter((item)=>item.id !== action.id)}
     }
     if(action.type==='clear'){
-        return (state=[])
+        return {...state , data : [] }
     }
 }
 
